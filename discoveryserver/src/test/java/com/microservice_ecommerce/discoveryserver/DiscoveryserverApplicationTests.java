a195f88cc4a57339f744d9b8b277e3644a8314f9
sachin <sachin.kumar09@hotmail.com>
2025-07-06 19:46:27 +0530

    chore: bootstraping done, pushing discoveryserver for phase 2

---
+++ discoveryserver/src/test/java/com/microservice_ecommerce/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice_ecommerce.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
